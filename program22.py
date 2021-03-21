deac728d19543675b4672b6108c01bc2978145d3
zopletalova <79161629+zopletalova@users.noreply.github.com>
2021-03-21 10:04:19 +0100

    Commit

--- program22.py
+++ program22.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 #import wget
 # wget.download("https://raw.githubusercontent.com/pesikj/python-012021/master/zadani/5/character-deaths.csv")
-DataFrame = pd.read_csv("character-deaths.csv", index_col="Name")
+df = pd.read_csv("character-deaths.csv", index_col="Name")
 
-print(DataFrame.info)
-print(DataFrame.columns)
+print(df.info)
+print(df.columns)
 
-d_hali = DataFrame.loc["Hali", "Death Year"]
+d_hali = df.loc["Hali", "Death Year"]
 print(f"Smrt Hali - rok: {int(d_hali)}")
 
-print(DataFrame.loc["Gevin Harlaw":"Gillam"])
+print(df.loc["Gevin Harlaw":"Gillam"])
 print()
 
-print(DataFrame.loc["Gevin Harlaw":"Gillam","Death Year"])
+print(df.loc["Gevin Harlaw":"Gillam","Death Year"])
 print()
 
-print(DataFrame.loc["Gevin Harlaw":"Gillam","GoT":"DwD"])
+print(df.loc["Gevin Harlaw":"Gillam","GoT":"DwD"])
