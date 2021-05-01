39a00df4f2385fcd3e494047db61b48ce6d3b110
zopletalova <79161629+zopletalova@users.noreply.github.com>
2021-05-01 22:43:02 +0200

    Commit

--- pujcovna/katalog/urls.py
+++ pujcovna/katalog/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('seznam/', views.SeznamView.as_view(), name='index1'),
+    path('auta/', views.SeznamAutView.as_view(), name='auta_list'),
+    path('zakaznici/', views.SeznamZakaznikuView.as_view(), name='zakaznici_list'),
+    path('vypujcky/', views.VypujckyPrehledView.as_view(), name='vypujcky_list'),
 ]
