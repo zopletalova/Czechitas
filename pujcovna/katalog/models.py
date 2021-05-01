bd7f6fb97e2d76791a019aeab7c301e697f204f0
zopletalova <79161629+zopletalova@users.noreply.github.com>
2021-05-01 22:42:01 +0200

    Commit

--- pujcovna/katalog/models.py
+++ pujcovna/katalog/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+import pytz
 
 class Auto(models.Model):
   RZ = models.CharField(max_length=100)
@@ -6,13 +8,45 @@ class Auto(models.Model):
   najeto_km = models.IntegerField()
   dat_technicke = models.DateField()
 
+  @property
+  def odtechnicke(self):
+    dnes = datetime.datetime.now()
+    dnes = pytz.UTC.localize(dnes)
+    dnes = datetime.datetime.date(dnes)
+    delka = dnes - self.dat_technicke
+    return delka.days
+
 class Zakaznik(models.Model):
+  PROGRAM_DRUHY = (
+    ('B', 'běžný zákazník'),
+    ('Z', 'zlatý program'),
+    ('P', 'platinový program'),
+  )
+
   jmeno = models.CharField(max_length=100)
   prijmeni = models.CharField(max_length=100)
   RP_cislo = models.CharField(max_length=20)
   dat_narozeni = models.DateField()
+  program = models.CharField(max_length=1, choices=PROGRAM_DRUHY, null=True)
 
 class Vypujcka(models.Model):
+  id_cislo = models.IntegerField()
   vypujceno = models.DateTimeField()
   vraceno = models.DateTimeField()
   cena = models.IntegerField()
+  auto = models.ForeignKey(Auto, on_delete=models.SET_NULL, null=True)
+  zakaznik = models.ForeignKey(Zakaznik, on_delete=models.SET_NULL, null=True)
+
+  @property
+  def urcistav(self):
+    ted = datetime.datetime.now()
+    ted = pytz.UTC.localize(ted)
+    vypujceno = self.vypujceno
+    vraceno = self.vypujceno
+    if (ted >= vypujceno) & (ted <= vraceno):
+      stav = "výpůjčka běží"
+    elif ted > vraceno:
+      stav = "výpůjčka skončila"
+    elif ted < vypujceno:
+      stav = "výpůjčka ještě nezačala"
+    return stav
