6d3b8b46181f143c6116b2086e3db5f542092cba
zopletalova <79161629+zopletalova@users.noreply.github.com>
2021-05-01 23:22:44 +0200

    Commit

--- pujcovna/katalog/urls.py
+++ pujcovna/katalog/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('auta/', views.SeznamAutView.as_view(), name='auta_list'),
+    path('autazak/', views.SeznamAutProZakView.as_view(), name='autazak_list'),
     path('zakaznici/', views.SeznamZakaznikuView.as_view(), name='zakaznici_list'),
     path('vypujcky/', views.VypujckyPrehledView.as_view(), name='vypujcky_list'),
 ]
--- pujcovna/katalog/views.py
+++ pujcovna/katalog/views.py
@@ -11,7 +11,7 @@ class IndexView(View):
     def get(self, request):
         return HttpResponse("<h1 align='center'><font color='fuchsia'> Vítejte v naší autopůjčovně!</font></h1>"
                             "<p align='center'>"
-                            "<a href='http://localhost:8000/katalog/seznam/'>Jaká auta máme?</a><br>"
+                            "<a href='http://localhost:8000/katalog/autazak/'>Jaká auta máme?</a><br>"
                             "</p>"
                             "<h2 align='center'>O naší autopůjčovně</h2>"
                             "<p align='center'>Naše půjčovna vznikla v roce 2011 a dnes nabízí přibližně 30 aut.</p>"
@@ -26,6 +26,11 @@ class IndexView(View):
 class SeznamAutView(ListView):
     model = models.Auto
     template_name = "auta/auta_list.html"
+
+class SeznamAutProZakView(ListView):
+    model = models.Auto
+    template_name = "auta/autazak_list.html"
+
 
 class SeznamZakaznikuView(ListView):
     model = models.Zakaznik
